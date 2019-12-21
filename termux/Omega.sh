f442c839a85074499829325c75f0bd9f8742ae53
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2019-12-20 21:02:35 -0700

    added ssh key gen

--- termux/Omega.sh
+++ termux/Omega.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Constatts for kik
-readonly KIK_PATH="/sdcard/Kik/"
-readonly REMOTE_KIK_PATH="/mnt/shareVol/2tb/Oasis/kik/"
-readonly OMEGA_USERNAME="joshua"
-readonly OMEGA_HOST="Omega.lan"
-
 ext_rename(){
     # Ensure necessary arguments are given
     if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
@@ -21,7 +15,7 @@ ext_rename(){
     dst_ext=."$2"
     path="$3"/
 
-    for file in "$path"/*"$src_ext"; 
+    for file in "$path"/*"$src_ext";
     do
         src="$file";
         dest="$path"/"$(basename "$file" "$src_ext")""$dst_ext";
@@ -36,7 +30,13 @@ ext_rename(){
 }
 
 # Moves all kik images to server and removes originals.
-kik-clean(){    
+kik-clean(){
+    # Static Paths
+    readonly KIK_PATH="/sdcard/Kik/"
+    readonly REMOTE_KIK_PATH="/mnt/shareVol/2tb/Oasis/kik/"
+    readonly OMEGA_USERNAME="joshua"
+    readonly OMEGA_HOST="Omega.lan"
+
     # Ensure an argument is given
     if [ -z "$1" ]
     then
@@ -49,13 +49,14 @@ kik-clean(){
     # Rename images to fix encoding
     ext_rename jpg png "$KIK_PATH"
     # Copy all files over to server
-    rsync -progress -ap -essh "$KIK_PATH" "$OMEGA_USERNAME"@"$OMEGA_HOST":"$dest_path"/;
-    # Delete files from phone
+    rsync --progress -ap -essh "$KIK_PATH" "$OMEGA_USERNAME"@"$OMEGA_HOST":"$dest_path"/;
+    # Delete local copy of files
     rm -f "$KIK_PATH"*
 
     return;
 }
 
+: << "END"
 update_alarm(){
     # Parameters to be adjusted
     EN_STATUS=true;
@@ -95,4 +96,4 @@ update_alarm(){
 
     return;
 }
-
+END
--- termux/install.sh
+++ termux/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -o errexit  # Exit on most errors
+set -o errtrace # Make sure any error trap is inherited
+set -o nounset  # Disallow expansion of unset variables
+set -o pipefail # Use last non-zero exit code in a pipeline
+#set -o xtrace  # Trace the execution of the script (debug)
+
 # GENERAL
 termux-setup-storage
 
@@ -16,4 +22,11 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ".vimrc" "$HOME"/
 vim +PluginInstall +qall
 
+# Generate and add ssh key to ssh-agent
+ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C "joshua.gf.arul@gmail.com" -N ''
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
 echo "Done"
+
+
