6fcbc23d0780715578268739e5866790660dee89
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2020-09-18 11:06:34 -0600

    :sparkles: Add more user-friendly indicators

--- fonts.sh
+++ fonts.sh
@@ -11,7 +11,12 @@ find "${fonts}/" -mindepth 1 -maxdepth 1 -type d -exec cp -r {} ${fontsDest}/ ';
 # Install nerdfonts if pacman is available
 if [[ -f "/etc/arch-release" ]]; then
     sudo pacman -S --noconfirm ttf-nerd-fonts-symbols noto-fonts-emoji
+else
+    echo "You're not running an Arch based distro!"
+    echo "Find and install ttf-nerd-fonts-symbols and noto-fonts-emoji yourself"
 fi
 
 # Reload font cache
 2>/dev/null 1>&2 fc-cache -f -v
+
+echo "Done installing fonts!"
