04e1acfadaac71f1206b4975ebb800038f7efe2c
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-01-08 01:11:02 -0700

    :art: Format and style fixes

--- ref/get_kernel_modules.py
+++ ref/get_kernel_modules.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 
-import sys
-from subprocess import Popen, PIPE
-
 import os
-from pprint import pprint
+import sys
+from subprocess import PIPE, Popen
+
 
 def get_makefiles(path):
-    p = Popen(["find", path, "-type", "f", "-name", "Makefile"], stdout=PIPE, stderr=PIPE)
+    """Find all the files named `Makefile` within the path specified."""
+    p = Popen(
+        ["find", path, "-type", "f", "-name", "Makefile"], stdout=PIPE, stderr=PIPE
+    )
     stdout, stderr = p.communicate()
     clean = stdout.decode().replace("'", "")
     return clean.split()
+
 
 KERNEL_PATH = "/usr/src/linux/"
 MAKE_PATHS = get_makefiles(KERNEL_PATH)
 
 print(f"KERNEL_PATH: {KERNEL_PATH}")
-#pprint(f"MAKEFILE_PATH: {MAKE_PATHS}")
+# print(f"MAKEFILE_PATH: {MAKE_PATHS}")
 
+# Either pipe into stdin in type in module names
 if sys.stdin.isatty():
     print("Enter modules or q to quit")
 
 for line in sys.stdin:
     line = line.lower().strip()
+    # Replace the file extension to that used in makefiles
     if ".ko" in line:
         real_name = "+= " + os.path.splitext(line)[0] + ".o"
         p = Popen(["grep", real_name, *MAKE_PATHS], stdout=PIPE, stderr=PIPE)
@@ -31,4 +36,3 @@ for line in sys.stdin:
     elif sys.stdin.isatty():
         if line.strip() == "q":
             exit(0)
-        
