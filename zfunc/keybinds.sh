e95b2e32b56585a2f8850cad89266581275c323e
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-01-22 14:12:24 -0700

    :art: Formatting

--- zfunc/keybinds.sh
+++ zfunc/keybinds.sh
@@ -2,29 +2,29 @@
 
 # ctrl-s adds sudo to start
 add_sudo() {
-    sudo_present="$(echo "$BUFFER" | cut -d " " -f1)"
-    if [[ $sudo_present = "sudo" ]]; then
-        BUFFER="$(echo "$BUFFER" | cut -d " " -f2-)"
-    else
-        BUFFER="sudo $BUFFER"
-    fi
-    zle end-of-line
+  sudo_present="$(echo "$BUFFER" | cut -d " " -f1)"
+  if [[ $sudo_present = "sudo" ]]; then
+    BUFFER="$(echo "$BUFFER" | cut -d " " -f2-)"
+  else
+    BUFFER="sudo $BUFFER"
+  fi
+  zle end-of-line
 }
 zle -N add_sudo
 bindkey "^s" add_sudo
 
-# Move up one directory with ctrl+k
+# ctrl-k moves up one directory
 up() {
-    BUFFER="cd .."
-    zle accept-line
+  BUFFER="cd .."
+  zle accept-line
 }
 zle -N up
 bindkey "^k" up
 
-# Kill all other termux sessions with ctrl+q
+# ctrl-q kills all other tmux sessions
 kill_other() {
-    tmux kill-session -a
-    tmux rename-session 0
+  tmux kill-session -a
+  tmux rename-session 0
 }
 zle -N kill_other
 bindkey "^q" kill_other
