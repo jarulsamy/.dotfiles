cf3085f06feb6f8318a00376b1f7c846d7f78168
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-06-14 01:42:18 -0600

    :sparkles: Add auto-install for dotEngine

--- install.sh
+++ install.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
 
-# Pick a package manager.
-declare -A osInfo;
-osInfo[/etc/debian_version]="apt-get install -y"
-osInfo[/etc/alpine-release]="apk --update add"
-osInfo[/etc/centos-release]="yum install -y"
-osInfo[/etc/fedora-release]="dnf install -y"
-osInfo[/etc/arch-release]="pacman -S --noconfirm"
+# Echo to stderr
+echoerr() { echo "$@" 1>&2; }
 
-for f in ${!osInfo[@]}
-do
-    if [[ -f $f ]];then
-        pac="${osInfo[$f]}"
-    fi
+# Display an error message and quit
+error() {
+  echoerr "Error, $1"
+  exit 1
+}
+
+# Pick a package manager.
+declare -A osInfo
+osInfo["/etc/debian_version"]="apt-get install -y"
+osInfo["/etc/alpine-release"]="apk --update add"
+osInfo["/etc/centos-release"]="yum install -y"
+osInfo["/etc/fedora-release"]="dnf install -y"
+osInfo["/etc/arch-release"]="pacman -S --noconfirm --needed"
+
+for f in ${!osInfo[@]}; do
+  if [[ -f $f ]]; then
+    pac="${osInfo[$f]}"
+  fi
 done
 
+# Install a package
 install() {
   which "$1" &>/dev/null
 
   if [ $? -ne 0 ]; then
-    echo "Installing: ${1}..."
+    echoerr "Installing: ${1}..."
     sudo ${pac} ${1}
   else
-    echo "Already installed: ${1}"
+    echoerr "Already installed: ${1}"
   fi
 }
 
@@ -39,6 +48,43 @@ install xclip
 install xdg-utils
 install shfmt
 install shellcheck
+install cmake
+install make
+install gcc
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install oh-my-zsh, if not already present
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echoerr "Skipping oh-my-zsh install, $HOME/.oh-my-zsh already exists."
+fi
+
+# Compile and install dotEngine, if it doesn't exist
+if [ ! -f "$HOME/.local/bin/dotEngine" ]; then
+  # Prep, clone and setup build dir
+  compile_dir="$(mktemp -d)"
+  build_dir="$compile_dir/build"
+  install_prefix="$HOME/.local"
+
+  cd "$compile_dir" || error "Couldn't install dotEngine"
+  git clone https://github.com/jarulsamy/dotEngine "$compile_dir"
+  mkdir -p "$build_dir" "$install_prefix"
+  cd "$build_dir" || error "Couldn't install dotEngine"
+
+  # Actual compilation and installation
+  cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="$install_prefix"
+  make
+  make install
+
+  # Fix permission of resulting binary
+  chmod +x "$install_prefix"/bin/dotEngine
+  # Add compiled libraries to search path
+  sudo ldconfig "$install_prefix"/lib
+
+  # Cleanup
+  rm -rf "$compile_dir"
+else
+  echoerr "Skipping dotEngine install, $HOME/.local/bin/dotEngine already exists."
+fi
+
+echo "DONE"
