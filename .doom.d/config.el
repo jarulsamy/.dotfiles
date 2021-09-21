6ed7b787b61975ee69f2a9f02c6cb30926e28164
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-09-21 16:57:23 -0600

    Fix broken evil

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -75,7 +75,6 @@
 ;; to hanle sub-projects.
 ;; You can specify one if you encounter the issue.
 (setq doom-modeline-project-detection 'auto)
-
 ;; Determines the style used by `doom-modeline-buffer-file-name'.
 ;;
 ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
@@ -91,72 +90,52 @@
 ;;   relative-to-project => lisp/comint.el
 ;;   file-name => comint.el
 ;;   buffer-name => comint.el<2> (uniquify buffer name)
-;;
 ;; If you are experiencing the laggy issue, especially while editing remote files
 ;; with tramp, please try `file-name' style.
 ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
 (setq doom-modeline-buffer-file-name-style 'auto)
-
 ;; Whether display icons in the mode-line.
 ;; While using the server mode in GUI, should set the value explicitly.
 (setq doom-modeline-icon (display-graphic-p))
-
 ;; Whether display the colorful icon for `major-mode'.
 ;; It respects `all-the-icons-color-icons'.
 (setq doom-modeline-major-mode-color-icon t)
-
 ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
 (setq doom-modeline-buffer-state-icon t)
-
 ;; Whether display the modification icon for the buffer.
 ;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
 (setq doom-modeline-buffer-modification-icon t)
-
 ;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
 (setq doom-modeline-unicode-fallback nil)
-
 ;; Whether display the minor modes in the mode-line.
 (setq doom-modeline-minor-modes nil)
-
 ;; If non-nil, a word count will be added to the selection-info modeline segment.
 (setq doom-modeline-enable-word-count nil)
-
 ;; Major modes in which to display word count continuously.
 ;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
 ;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
 ;; remove the modes from `doom-modeline-continuous-word-count-modes'.
 (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
-
 ;; Whether display the buffer encoding.
 (setq doom-modeline-buffer-encoding t)
-
 ;; Whether display the indentation information.
 (setq doom-modeline-indent-info nil)
-
 ;; If non-nil, only display one number for checker information if applicable.
 (setq doom-modeline-checker-simple-format t)
-
 ;; The maximum number displayed for notifications.
 (setq doom-modeline-number-limit 99)
-
 ;; The maximum displayed length of the branch name of version control.
 (setq doom-modeline-vcs-max-length 20)
-
 ;; Whether display the workspace name. Non-nil to display in the mode-line.
 (setq doom-modeline-workspace-name t)
-
 ;; Whether display the perspective name. Non-nil to display in the mode-line.
 (setq doom-modeline-persp-name t)
-
 ;; If non nil the default perspective name is displayed in the mode-line.
 (setq doom-modeline-display-default-persp-name nil)
-
 ;; If non nil the perspective name is displayed alongside a folder icon.
 (setq doom-modeline-persp-icon t)
-
 ;; Whether display the `lsp' state. Non-nil to display in the mode-line.
 (setq doom-modeline-lsp t)
-
 ;; Whether display the modal state icon.
 ;; Including `evil', `overwrite', `god', `ryo' and `xah-fly-keys', etc.
 (setq doom-modeline-modal-icon t)
@@ -164,6 +143,12 @@
 ;; Keybinds
 ;; Quickly switch between corresponding files
 (global-set-key (kbd "C-x C-o") 'ff-find-other-file)
+
+;;; Modules
+;;; :editor evil
+;; Focus new window after splitting
+(setq evil-split-below t
+      evil-vsplit-window-right t)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -30,8 +30,8 @@
        ;;deft              ; notational velocity for Emacs
        doom                ; what makes DOOM look the way it does
        doom-dashboard      ; a nifty splash screen for Emacs
-       ;; doom-quit        ; DOOM quit-message prompts when you quit Emacs
-       ;; (emoji +unicode) ; 🙂
+       doom-quit           ; DOOM quit-message prompts when you quit Emacs
+       ;;(emoji +unicode)  ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
@@ -44,7 +44,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        tabs                ; a tab bar for Emacs
        treemacs            ; a project drawer, like neotree but cooler
-       ;; unicode          ; extended unicode support for various languages
+       unicode             ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        window-select       ; visually switch windows
@@ -59,7 +59,7 @@
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
-       objed               ; text object editing for the innocent
+       ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        rotate-text         ; cycle region at point between text candidates
        snippets            ; my elves. They type so I don't have to
