6bd2e8baac3bd4028df6ba6e470f986fc353abb5
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-12-17 00:48:15 -0700

    General cleanup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,38 +1,15 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Joshua Arulsamy"
       user-mail-address "joshua.gf.arul@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; Cascadia Code no longer has poor scrolling performance issues!
+(setq doom-font (font-spec :family "Cascadia Code" :size 14 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "Cascadia Code" :size 14))
 
-;; Normally prefer either Fira Code or Cascadia Code, but the latter suffers
-;; from __very__ poor scrolling performance.
-(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "Fira Code" :size 14))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'gruvbox-dark-hard)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+(setq doom-theme 'doom-gruvbox)
 (setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
@@ -68,77 +45,27 @@
 (setq +doom-dashboard-ascii-banner-fn #'doom-dashboard-draw-ascii-emacs-banner-fn)
 
 ;; Doom-Modeline
-;; How to detect the project root.
-;; nil means to use `default-directory'.
-;; The project management packages have some issues on detecting project root.
-;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
-;; to hanle sub-projects.
-;; You can specify one if you encounter the issue.
-(setq doom-modeline-project-detection 'auto)
-;; Determines the style used by `doom-modeline-buffer-file-name'.
-;;
-;; Given ~/Projects/FOSS/emacs/lisp/comint.el
-;;   auto => emacs/lisp/comint.el (in a project) or comint.el
-;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
-;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
-;;   truncate-with-project => emacs/l/comint.el
-;;   truncate-except-project => ~/P/F/emacs/l/comint.el
-;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
-;;   truncate-all => ~/P/F/e/l/comint.el
-;;   truncate-nil => ~/Projects/FOSS/emacs/lisp/comint.el
-;;   relative-from-project => emacs/lisp/comint.el
-;;   relative-to-project => lisp/comint.el
-;;   file-name => comint.el
-;;   buffer-name => comint.el<2> (uniquify buffer name)
-;; If you are experiencing the laggy issue, especially while editing remote files
-;; with tramp, please try `file-name' style.
-;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-(setq doom-modeline-buffer-file-name-style 'auto)
-;; Whether display icons in the mode-line.
-;; While using the server mode in GUI, should set the value explicitly.
-(setq doom-modeline-icon (display-graphic-p))
-;; Whether display the colorful icon for `major-mode'.
-;; It respects `all-the-icons-color-icons'.
-(setq doom-modeline-major-mode-color-icon t)
-;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
-(setq doom-modeline-buffer-state-icon t)
-;; Whether display the modification icon for the buffer.
-;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
-(setq doom-modeline-buffer-modification-icon t)
-;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
-(setq doom-modeline-unicode-fallback nil)
-;; Whether display the minor modes in the mode-line.
-(setq doom-modeline-minor-modes nil)
-;; If non-nil, a word count will be added to the selection-info modeline segment.
-(setq doom-modeline-enable-word-count nil)
-;; Major modes in which to display word count continuously.
-;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
-;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
-;; remove the modes from `doom-modeline-continuous-word-count-modes'.
-(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
-;; Whether display the buffer encoding.
-(setq doom-modeline-buffer-encoding t)
-;; Whether display the indentation information.
-(setq doom-modeline-indent-info nil)
-;; If non-nil, only display one number for checker information if applicable.
-(setq doom-modeline-checker-simple-format t)
-;; The maximum number displayed for notifications.
-(setq doom-modeline-number-limit 99)
-;; The maximum displayed length of the branch name of version control.
-(setq doom-modeline-vcs-max-length 20)
-;; Whether display the workspace name. Non-nil to display in the mode-line.
-(setq doom-modeline-workspace-name t)
-;; Whether display the perspective name. Non-nil to display in the mode-line.
-(setq doom-modeline-persp-name t)
-;; If non nil the default perspective name is displayed in the mode-line.
-(setq doom-modeline-display-default-persp-name nil)
-;; If non nil the perspective name is displayed alongside a folder icon.
-(setq doom-modeline-persp-icon t)
-;; Whether display the `lsp' state. Non-nil to display in the mode-line.
-(setq doom-modeline-lsp t)
-;; Whether display the modal state icon.
-;; Including `evil', `overwrite', `god', `ryo' and `xah-fly-keys', etc.
-(setq doom-modeline-modal-icon t)
+(setq doom-modeline-project-detection 'auto
+      doom-modeline-buffer-file-name-style 'auto
+      doom-modeline-icon (display-graphic-p)
+      doom-modeline-major-mode-color-icon t
+      doom-modeline-buffer-state-icon t
+      doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode)
+      doom-modeline-buffer-encoding t
+      doom-modeline-indent-info nil
+      doom-modeline-checker-simple-format t
+      doom-modeline-number-limit 99
+      doom-modeline-vcs-max-length 20
+      doom-modeline-workspace-name t
+      doom-modeline-persp-name t
+      doom-modeline-display-default-persp-name nil
+      doom-modeline-persp-icon t
+      doom-modeline-lsp t
+      doom-modeline-modal-icon t
+      doom-modeline-buffer-modification-icon t
+      doom-modeline-unicode-fallback nil
+      doom-modeline-minor-modes nil
+      doom-modeline-enable-word-count nil)
 
 ;; Keybinds
 ;; Quickly switch between corresponding files
@@ -168,63 +95,18 @@
   (setq company-show-numbers t)
   (add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less annoying.
 
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-
-(use-package! calctex
-  :commands calctex-mode
-  :init
-  (add-hook 'calc-mode-hook #'calctex-mode)
-  :config
-  (setq calctex-additional-latex-packages "
-        \\usepackage[usenames]{xcolor}
-        \\usepackage{soul}
-        \\usepackage{adjustbox}
-        \\usepackage{amsmath}
-        \\usepackage{amssymb}
-        \\usepackage{siunitx}
-        \\usepackage{cancel}
-        \\usepackage{mathtools}
-        \\usepackage{mathalpha}
-        \\usepackage{xparse}
-        \\usepackage{arevmath}"
-        calctex-additional-latex-macros
-        (concat calctex-additional-latex-macros
-                "\n\\let\\evalto\\Rightarrow"))
-  (defadvice! no-messaging-a (orig-fn &rest args)
-    :around #'calctex-default-dispatching-render-process
-    (let ((inhibit-message t) message-log-max)
-      (apply orig-fn args)))
-  ;; Fix hardcoded dvichop path (whyyyyyyy)
-  (let ((vendor-folder (concat (file-truename doom-local-dir)
-                               "straight/"
-                               (format "build-%s" emacs-version)
-                               "/calctex/vendor/")))
-    (setq calctex-dvichop-sty (concat vendor-folder "texd/dvichop")
-          calctex-dvichop-bin (concat vendor-folder "texd/dvichop")))
-  (unless (file-exists-p calctex-dvichop-bin)
-    (message "CalcTeX: Building dvichop binary")
-    (let ((default-directory (file-name-directory calctex-dvichop-bin)))
-      (call-process "make" nil nil nil))))
-
+;; Wakatime
 (global-wakatime-mode)
 (setq-default wakatime-cli-path "/home/joshua/.wakatime/wakatime-cli-linux-amd64")
+
+;; Clangd LSP
+(setq lsp-clients-clangd-args '("--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"
+                                "-j=4"
+                                "--pch-storage=memory"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
 ;;; config.el ends here
