108a658c8afdead8b9ec0c2314d0b27adea6fe9d
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-01-26 09:02:29 -0700

    Auto close compilation buffer if successful

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -205,3 +205,19 @@
                       (smtpmail-smtp-service         . 587)
                       )
                     t)
+
+
+;; Compiling
+(defun notify-compilation-result(buffer msg)
+  "Notify that the compilation is finished,
+close the *compilation* buffer if the compilation is successful,
+and set the focus back to Emacs frame"
+  (if (string-match "^finished" msg)
+      (progn (delete-windows-on buffer)))
+  (setq current-frame (car (car (cdr (current-frame-configuration)))))
+  (select-frame-set-input-focus current-frame)
+  )
+
+;; Bury the compilation buffer when compilation is done/successful.
+(add-to-list 'compilation-finish-functions
+             'notify-compilation-result)
