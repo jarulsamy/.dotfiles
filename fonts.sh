69117426e9295493a00159c314bb0370674b1fbb
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-04-06 09:20:08 -0600

    Fonts rework
    + Use the system package manager to install fonts whenever possible.
    + Remove unused fonts as a result of the previous rework.
    + Make the fonts installation script a bit more robust.

--- fonts.sh
+++ fonts.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
-fonts="$(pwd)/fonts"
+# Assume the fonts directory is where it should be.
+fonts="$HOME/.dotfiles/fonts"
 fontsDest="$HOME/.local/share/fonts"
+
+# Ensure the fonts directory is where it should be.
+if [ ! -d "$fonts" ]; then
+  printf "Can't find source fonts directory. Was .dotfiles cloned to \$HOME?\n"
+  exit 1
+fi
 
 # Ensure dest exists
 mkdir -p "${fontsDest}"
 
 # Copy font files over
-find "${fonts}/" -mindepth 1 -maxdepth 1 -type f -exec cp {} "${fontsDest}/" ';'
-find "${fonts}/" -mindepth 1 -maxdepth 1 -type d -exec cp -r {} "${fontsDest}"/ ';'
+find "${fonts}/" -mindepth 1 -maxdepth 1 -exec cp -r {} "${fontsDest}/" ';'
 
 # Install nerdfonts if pacman is available
+pacmanFonts=("ttf-fira-code" "ttf-jetbrains-mono" "ttf-cascadia-code" "ttf-nerd-fonts-symbols" "noto-fonts-emoji")
 if [[ -f "/etc/arch-release" ]]; then
-  sudo pacman -S --noconfirm ttf-fira-code ttf-nerd-fonts-symbols noto-fonts-emoji
+  for i in "${pacmanFonts[@]}"; do
+    sudo pacman -S --noconfirm --needed "$i"
+  done
 else
-  echo "You're not running an Arch based distro!"
-  echo "Install nerd fonts, noto fonts, and fira code yourself."
+  printf "You're not running an Arch based distro!\n"
+  printf "Install the following fonts yourself:\n"
+  for i in "${pacmanFonts[@]}"; do
+    printf "  * %s\n" "$i"
+  done
 fi
 
 # Reload font cache
 fc-cache 2>/dev/null 1>&2 -f -v
 
-echo "Done installing fonts!"
+printf "Done installing fonts!\n"
